881f8a5633b0b8171c84013b12134789a116ac0d
Thorin Farnsworth <thorin.j.a.farnsworth@gmail.com>
2024-05-17 21:44:34 +0000

    toggledac cleanup

--- Experiments/02ToggleDAC/main.c
+++ Experiments/02ToggleDAC/main.c
@@ -16,15 +16,15 @@
 #define CLR_PORT PORTC
 #define CONTROL_DDR DDRC
 
-// Macros for setting and clearing bits
-#define SET_BIT(PORT, PIN) ((PORT) |= (1 << (PIN)))
-#define CLEAR_BIT(PORT, PIN) ((PORT) &= ~(1 << (PIN)))
-
 // MAX5825 DAC Definitions
 #define MAX5825_ADDR 0x20 // 0010 000W, where W is the write bit
 #define MAX5825_REG_REF 0x20
 #define MAX5825_REG_CODEn_LOADn 0xB0 // 1011 XXXX, where X encodes the DAC channel
 #define MAX5825_REG_CODEn_LOADall 0xA0
+
+// Macros for setting and clearing bits
+#define SET_BIT(PORT, PIN) ((PORT) |= (1 << (PIN)))
+#define CLEAR_BIT(PORT, PIN) ((PORT) &= ~(1 << (PIN)))
 
 // Function prototypes
 void setup(void);
@@ -39,7 +39,6 @@ void setup() {
     CONTROL_DDR |= (1 << LDAC_PIN) | (1 << CLR_PIN); // Set LDAC and CLR as outputs 
     // Set LDAC high to hold the DAC output (active-low signal, high means no update)
     LDAC_PORT |= (1 << LDAC_PIN); 
-
     // Set CLR high to avoid clearing the DAC (active-low signal, high means no clear)
     CLR_PORT |= (1 << CLR_PIN); 
 
@@ -93,11 +92,6 @@ void max5825_write(uint8_t channel, uint16_t value) {
     // The lower 4 bits of the value (LSB) shifted into the upper 4 bits of the byte, as the MAX5825 expects it
     twi_write((uint8_t)(value & 0xF0));
     twi_stop();
-
-    // Toggle LDAC to update the DAC output
-    CLEAR_BIT(LDAC_PORT, LDAC_PIN); // Set LDAC low to trigger the update
-    _delay_us(10); // Wait for the DAC to update
-    SET_BIT(LDAC_PORT, LDAC_PIN); // Set LDAC high again
 }
 
 int main(void) {
